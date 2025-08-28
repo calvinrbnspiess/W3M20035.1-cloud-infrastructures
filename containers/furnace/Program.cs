9081dd0e706c5a3aabd1cdedaa305700819ec4c4
Nico W <nico.wolffld@gmx.de>
2025-08-28 21:03:40 +0200

    add health checks to furnace/oven + readme

--- containers/furnace/Program.cs
+++ containers/furnace/Program.cs
@@ -1,4 +1,5 @@
 using furnace;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -9,12 +10,26 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<PizzaOvenService>();
 
+builder.Services.AddHealthChecks()
+    .AddCheck<PizzaOvenHealthCheck>("pizza_oven");;
+
 var app = builder.Build();
 
 if (app.Environment.IsProduction())
 {
     app.Urls.Add("http://0.0.0.0:8080");
 }
+
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = _ => false
+});
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
+{
+    Predicate = check => check.Name == "pizza_oven"
+});
 
 // Swagger
 app.UseSwagger();
